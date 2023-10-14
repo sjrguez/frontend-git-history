ce65a86e689658b0a3d5dd3df37fd8ce68fd5877
starlin de la cruz <starlinr82@gmail.com>
2023-10-13 23:24:13 -0400

    Add: getBranchByName added on branchService

--- src/app/branch/branch.service.ts
+++ src/app/branch/branch.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Branch } from './models/branch.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,11 @@ export class BranchService {
   private url = 'http://localhost:5000/api/branch'
   constructor(private http: HttpClient) {}
 
-  getAllBranches() {
+  getAllBranches(): Observable<Branch[]> {
     return this.http.get<Branch[]>(this.url)
+  }
+
+  getBranchByName(name: string): Observable<Branch> {
+    return this.http.get<Branch>(`${this.url}/${name}`)
   }
 }
